2acfc27fea90c68c3b54f4c302cfe8938a8d04d4
Vinay Nath <vinayfour@gmail.com>
2025-11-21 18:58:44 +1100

    Initial commit - Fitness3k site

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,23 +1,23 @@
 module.exports = {
-    name: "Code Stitch Web Designs",
-    email: "help@codestitch.app",
-    phoneForTel: "555-779-4407",
-    phoneFormatted: "(555) 779-4407",
+    name: "Fitness 3K",
+    email: "join@fitness3K.co",
+    phoneForTel: "03 9213 2222",
+    phoneFormatted: "(03) 9213 2222",
     address: {
-        lineOne: "First Address Line",
-        lineTwo: "Second Address Line",
-        city: "Denver",
-        state: "CO",
-        zip: "80206",
-        country: "US",
-        mapLink: "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6",
+        lineOne: "9 Fullerton Ln",
+        lineTwo: "",
+        city: "Melbourne",
+        state: "VIC",
+        zip: "3000",
+        country: "AU",
+        mapLink: "https://maps.app.goo.gl/ATWwXHDtoHjPS8En6",
     },
     socials: {
         facebook: "https://www.facebook.com/",
         instagram: "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://www.example.com",
+    domain: "https://www.fitness3k.co",
     // Passing the isProduction variable for use in HTML templates
     isProduction: process.env.ELEVENTY_ENV === "PROD",
 };
